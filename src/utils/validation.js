f14dde45cf8292e768012516cdec3c692a6af705
abhay-rathod <abhayr.tagline@gmail.com>
2024-05-22 18:39:25 +0530

    validation

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,32 +1,35 @@
-export const validation = (name, value, state, storeState, currUser) => {
+import { checkIncludes, objectValues } from "./javaScript";
 
+export const validation = (name, value, state, storeState, currUser) => {
     if (value.toString().length <= 0 || value === 0 || value < 0) {
-        // return "Enter value to submit.";
         return "Enter value greater than 0.";
     } else if (value.toString().length > 7) {
-        return "7 digit error.";
-        // return "Number may only have 7 digits.";
-    } else if (Object.values(state)?.includes(Number(value)) || storeState[currUser]?.includes(Number(value))) {
-        // return "This value is already entered.";
+        return "Enter 7 digit only.";
+    } else if (
+        checkIncludes(objectValues(state), Number(value)) ||
+        checkIncludes(storeState[currUser], Number(value))
+    ) {
         return "Repeated value.";
     }
 };
 
 
 export const validateAll = (state, currUser, cardLabel, errors, setErrors) => {
-
-    const erroEmpty = {};
-    const inputNo = ['one', 'two', 'three', 'four', 'five', 'six'];
+    const errorEmpty = {};
+    const inputNo = ["one", "two", "three", "four", "five", "six"];
 
     for (let field of inputNo) {
-        if (!(state[`${currUser}-${cardLabel}-${field}`])) {
-            erroEmpty[`${currUser}-${cardLabel}-${field}`] = "This field is required.";
+        if (!state[`${currUser}-${cardLabel}-${field}`]) {
+            errorEmpty[`${currUser}-${cardLabel}-${field}`] =
+                "This field is required.";
         }
     }
 
-    if (Object.values(erroEmpty).filter((item) => item !== undefined).length) {
-        setErrors({ ...errors, ...erroEmpty });
+    if (objectValues(errorEmpty).filter((item) => item !== undefined).length) {
+        setErrors({ ...errors, ...errorEmpty });
     }
 
-    return Object.values(erroEmpty).filter((item) => item !== undefined).length === 0;
-}
+    return (
+        objectValues(errorEmpty).filter((item) => item !== undefined).length === 0
+    );
+};
